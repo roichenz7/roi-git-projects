86a6b307b98612add05857e12b381ecb48547c55
Roi <roichenz7@gmail.com>
2014-04-15 19:32:07 +0300

    initial

---
+++ download-manager/pre-dl/src/main/java/PreDL.java
@@ -0,0 +1,13 @@
+
+public class PreDL implements Runnable {
+
+    private final String configFilename;
+
+    public PreDL(String configFilename) {
+        this.configFilename = configFilename;
+    }
+
+    @Override
+    public void run() {
+    }
+}
