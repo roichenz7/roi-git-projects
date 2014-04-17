5bf2e2bb8d783e96c036f321bec8d09ca142dc93
Roi <roichenz7@gmail.com>
2014-04-17 13:11:16 +0300

    test

--- download-manager/my-episodes-api/src/test/java/MyEpisodesTest.java
+++ download-manager/my-episodes-api/src/test/java/MyEpisodesTest.java
@@ -8,4 +8,16 @@ public class MyEpisodesTest {
     public void testLogin() {
         MyEpisodesService myEpisodesService = new MyEpisodesServiceImpl("roichenz7", "7roi7roi");
     }
+
+    @Test
+    public void testMarkAsAcquired() {
+        MyEpisodesService myEpisodesService = new MyEpisodesServiceImpl("roichenz7", "7roi7roi");
+        myEpisodesService.markAsAcquired(8353, 1, 1);
+    }
+
+    @Test
+    public void testMarkAsWatched() {
+        MyEpisodesService myEpisodesService = new MyEpisodesServiceImpl("roichenz7", "7roi7roi");
+        myEpisodesService.markAsWatched(8353, 1, 1);
+    }
 }
