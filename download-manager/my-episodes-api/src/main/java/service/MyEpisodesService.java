884d0563ce95f65b3a97827765f37bcc41cc403c
Roi <roichenz7@gmail.com>
2014-04-17 13:14:34 +0300

    com

--- download-manager/my-episodes-api/src/main/java/service/MyEpisodesService.java
+++ download-manager/my-episodes-api/src/main/java/service/MyEpisodesService.java
@@ -2,7 +2,21 @@ package service;
 
 public interface MyEpisodesService {
 
+    /**
+     * Marks given episode as acquired
+     *
+     * @param showId show id
+     * @param season season number
+     * @param episode episode number
+     */
     void markAsAcquired(int showId, int season, int episode);
 
+    /**
+     * Marks given episode as watched
+     *
+     * @param showId show id
+     * @param season season number
+     * @param episode episode number
+     */
     void markAsWatched(int showId, int season, int episode);
 }
