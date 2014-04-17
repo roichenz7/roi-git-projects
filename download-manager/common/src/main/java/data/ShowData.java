35452fd1c19d4034dcea6dbcb00420774028c20e
Roi <roichenz7@gmail.com>
2014-04-17 18:06:17 +0300

    data

--- download-manager/common/src/main/java/data/ShowData.java
+++ download-manager/common/src/main/java/data/ShowData.java
@@ -2,23 +2,36 @@ package data;
 
 public class ShowData {
 
-    public ShowData() {
-    }
+    private String title;
+    private String season;
+    private int seasonNumber;
+    private int episodeNumber;
 
-    public void setTitle(String title) {
-        this.title = title;
+    public ShowData() {
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setSeason(String season) {
-        this.season = season;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getSeason() {
         return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
     }
 
     public boolean isEmpty() {
@@ -39,10 +52,25 @@ public class ShowData {
             if (temp.matches("S[0-9][0-9](E[0-9][0-9])+")) { // found season & episode/s
                 char c1 = temp.charAt(1);
                 char c2 = temp.charAt(2);
-                if (c1 == '0')
+                if (c1 == '0') {
                     showData.setSeason("Season " + c2);
-                else
+                    showData.seasonNumber = c2;
+                }
+                else {
                     showData.setSeason("Season " + c1 + c2);
+                    showData.seasonNumber = c1 + c2;
+                }
+
+                // TODO: handle multiple episodes...
+                int index = temp.indexOf("E");
+                char c3 = temp.charAt(index + 1);
+                char c4 = temp.charAt(index + 2);
+                if (c3 == '0') {
+                    showData.episodeNumber = c3;
+                }
+                else {
+                    showData.episodeNumber = c3 + c4;
+                }
 
                 showData.setTitle(title.trim());
                 return showData;
@@ -53,7 +81,4 @@ public class ShowData {
 
         return showData;
     }
-
-    private String title;
-    private String season;
 }
