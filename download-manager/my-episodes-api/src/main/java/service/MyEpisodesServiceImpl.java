a4ec95b6af5b4334cf41e2900fb880684695a2fe
Roi <roichenz7@gmail.com>
2014-04-17 12:23:17 +0300

    login

--- download-manager/my-episodes-api/src/main/java/service/MyEpisodesServiceImpl.java
+++ download-manager/my-episodes-api/src/main/java/service/MyEpisodesServiceImpl.java
@@ -2,11 +2,13 @@ package service;
 
 import com.ning.http.client.cookie.Cookie;
 import exceptions.LoginException;
+import http.CookieFactory;
 import http.HttpMethod;
 import http.HttpRequestBuilder;
 import http.IHttpResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyEpisodesServiceImpl implements MyEpisodesService {
 
@@ -30,21 +32,18 @@ public class MyEpisodesServiceImpl implements MyEpisodesService {
     private void login(final String username, final String password) {
         IHttpResponse response;
         try {
+            final String body = String.format("username=%s&password=%s&action=Login&u=", username, password);
             response = new HttpRequestBuilder(HttpMethod.POST, baseUrl + "/login.php")
-                    .withUrlParam("username", username)
-                    .withUrlParam("pass", password)
-                    .withUrlParam("action", "Login")
-                    .withUrlParam("u", "")
+                    .withHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .withBody(body)
                     .execute();
         } catch (Exception e) {
             throw new LoginException(username, e);
         }
 
-        if (response.getStatusCode() != 200 || !response.getStatusText().equals("OK")) {
-            throw new LoginException(username, "Http response: " + response);
-        }
-
-        List<String> headers = response.getHeaders("Set-Cookie");
-        // TODO: save cookies
+        cookies = response.getHeaders("Set-Cookie")
+                .stream()
+                .map(CookieFactory::create)
+                .collect(Collectors.toList());
     }
 }
