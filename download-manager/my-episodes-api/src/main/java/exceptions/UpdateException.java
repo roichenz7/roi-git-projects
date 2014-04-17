2fbaf3843a27da63a272f9fac015692cb3b819d7
Roi <roichenz7@gmail.com>
2014-04-17 17:29:03 +0300

    exc

--- download-manager/my-episodes-api/src/main/java/exceptions/UpdateException.java
+++ download-manager/my-episodes-api/src/main/java/exceptions/UpdateException.java
@@ -3,14 +3,14 @@ package exceptions;
 public class UpdateException extends RuntimeException {
 
     public UpdateException(int showId, int season, int episode, Throwable cause) {
-        super(String.format("Update of show %d, season %d, episode %d failed", showId, season, episode), cause);
+        super(String.format("Update of show: %d, season %d, episode %d failed", showId, season, episode), cause);
     }
 
     public UpdateException(int showId, int season, int episode, String message) {
-        super(String.format("Update of show %d, season %d, episode %d failed. %s", showId, season, episode, message));
+        super(String.format("Update of show: %d, season %d, episode %d failed. %s", showId, season, episode, message));
     }
 
     public UpdateException(String showName, int season, int episode, String message) {
-        super(String.format("Update of show %s, season %d, episode %d failed. %s", showName, season, episode, message));
+        super(String.format("Update of show: %s, season %d, episode %d failed. %s", showName, season, episode, message));
     }
 }
