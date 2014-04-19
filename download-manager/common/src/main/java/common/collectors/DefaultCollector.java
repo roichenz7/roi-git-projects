4467d53a1bb79b0fd792d495fb2f61356e97922b
Roi <roichenz7@gmail.com>
2014-04-19 19:37:30 +0300

    collector

---
+++ download-manager/common/src/main/java/common/collectors/DefaultCollector.java
@@ -0,0 +1,72 @@
+package common.collectors;
+
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * Default implementation for {@code Collector}.
+ *
+ * @param <T> the type of elements to be collected
+ * @param <R> the type of the result
+ */
+public class DefaultCollector<T, A, R> implements Collector<T, A, R> {
+
+    private final Supplier<A> supplier;
+    private final BiConsumer<A, T> accumulator;
+    private final BinaryOperator<A> combiner;
+    private final Function<A, R> finisher;
+    private final Set<Characteristics> characteristics;
+
+    public DefaultCollector(Supplier<A> supplier,
+                            BiConsumer<A, T> accumulator,
+                            BinaryOperator<A> combiner,
+                            Function<A, R> finisher,
+                            Set<Characteristics> characteristics) {
+        this.supplier = supplier;
+        this.accumulator = accumulator;
+        this.combiner = combiner;
+        this.finisher = finisher;
+        this.characteristics = characteristics;
+    }
+
+    public DefaultCollector(Supplier<A> supplier,
+                            BiConsumer<A, T> accumulator,
+                            BinaryOperator<A> combiner,
+                            Set<Characteristics> characteristics) {
+        this(supplier, accumulator, combiner, castingIdentity(), characteristics);
+    }
+
+    @Override
+    public BiConsumer<A, T> accumulator() {
+        return accumulator;
+    }
+
+    @Override
+    public Supplier<A> supplier() {
+        return supplier;
+    }
+
+    @Override
+    public BinaryOperator<A> combiner() {
+        return combiner;
+    }
+
+    @Override
+    public Function<A, R> finisher() {
+        return finisher;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return characteristics;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <I, R> Function<I, R> castingIdentity() {
+        return i -> (R) i;
+    }
+}
