cdce3c5ee08c3247c5b7204764f97dec63ca7390
Roi <roichenz7@gmail.com>
2014-04-21 19:17:16 +0300

    doc factory

--- commons/src/main/java/data/serializers/XmlSerializer.java
+++ commons/src/main/java/data/serializers/XmlSerializer.java
@@ -5,35 +5,19 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.IOException;
-import java.io.InputStream;
+import xml.DocumentFactory;
 
 public abstract class XmlSerializer<T> implements ISerializer<T> {
 
     @Override
     public final void deserialize(String filename, IDataParser<T> dataParser) {
-        InputStream inputStream = null;
         try {
-            inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(filename);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputStream);
+            Document doc = DocumentFactory.createFromFilename(filename);
             Element root = doc.getDocumentElement();
             root.normalize();
             deserialize(root, dataParser);
         } catch (Exception e) {
             throw new RuntimeException("Failed de-serializing from configuration file: " + filename, e);
-        } finally {
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-            } catch (IOException e) {
-                // Empty
-            }
         }
     }
 
