0563f85c87c346f0b9d9d9f79a1633372104bc98
Roi <roichenz7@gmail.com>
2014-04-21 16:54:24 +0300

    toStr

--- download-manager/pre-dl/src/main/java/data/ResultData.java
+++ download-manager/pre-dl/src/main/java/data/ResultData.java
@@ -1,16 +1,21 @@
 package data;
 
+import enums.Quality;
+
 public class ResultData {
 
     private String tvShowName;
     private int season;
     private int episode;
+    private Quality quality;
 
     private int seeds;
     private int peers;
 
     private String origin;
     private boolean isProper;
+
+    private String downloadLink;
 
     public ResultData(String source) {
         // TODO: init fields
@@ -28,6 +33,10 @@ public class ResultData {
         return episode;
     }
 
+    public Quality getQuality() {
+        return quality;
+    }
+
     public int getSeeds() {
         return seeds;
     }
@@ -36,7 +45,20 @@ public class ResultData {
         return peers;
     }
 
+    public String getOrigin() {
+        return origin;
+    }
+
     public boolean isProper() {
         return isProper;
+    }
+
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s.S%02dE%02d.%s", tvShowName, season, episode, quality);
     }
 }
