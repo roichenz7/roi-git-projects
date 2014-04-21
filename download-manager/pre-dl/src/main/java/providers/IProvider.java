3f2ad870ea74e5ce6b32da8597a85428da9f7075
Roi <roichenz7@gmail.com>
2014-04-21 14:33:30 +0300

    com

--- download-manager/pre-dl/src/main/java/providers/IProvider.java
+++ download-manager/pre-dl/src/main/java/providers/IProvider.java
@@ -8,10 +8,28 @@ import java.util.List;
 
 public interface IProvider {
 
+    /**
+     * @return provider name
+     */
     String getName();
 
+    /**
+     * Performs search according to given parameters
+     *
+     * @param tvShowName tv show name
+     * @param season season number
+     * @param episode episode number
+     * @param quality file quality
+     * @return list of results
+     */
     List<ResultData> search(String tvShowName, int season, int episode, Quality quality);
 
+    /**
+     * Returns best result from given results
+     *
+     * @param results results
+     * @return best result
+     */
     default ResultData getBestResult(List<ResultData> results) {
         return results.stream()
                 .filter(d -> d.getSeeds() > 100)
@@ -19,5 +37,10 @@ public interface IProvider {
                 .orElseThrow(ResultNotFoundException::new);
     }
 
+    /**
+     * Downloads file according to given result
+     *
+     * @param resultData result
+     */
     void downloadFile(ResultData resultData);
 }
