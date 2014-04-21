fee0d2c3e1c85e97e49462cb52384d8226a3460f
Roi <roichenz7@gmail.com>
2014-04-21 14:26:40 +0300

    test

---
+++ download-manager/pre-dl/src/test/java/PreDLTest.java
@@ -0,0 +1,22 @@
+import data.ResultData;
+import enums.Quality;
+import org.junit.Test;
+import providers.*;
+
+import java.util.List;
+
+public class PreDLTest {
+
+    @Test
+    public void testKat() {
+        IProvider provider = new KatProvider();
+        List<ResultData> results = provider.search("Arrow", 2, 18, Quality.HD_720p);
+        ResultData result = provider.getBestResult(results);
+        provider.downloadFile(result);
+    }
+
+    @Test
+    public void testPhd() {
+        IProvider provider = new PhdProvider();
+    }
+}
