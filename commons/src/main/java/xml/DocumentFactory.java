17a6faa90d8943355f886b023e255333ac5b9c1c
Roi <roichenz7@gmail.com>
2014-04-21 19:17:10 +0300

    doc factory

---
+++ commons/src/main/java/xml/DocumentFactory.java
@@ -0,0 +1,59 @@
+package xml;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+
+public class DocumentFactory {
+
+    /**
+     * Creates an xml document from source string
+     *
+     * @param source source string
+     * @return xml document
+     */
+    public static Document createFromString(String source) {
+        Document doc;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(new InputSource(new StringReader(source)));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create xml document from source", e);
+        }
+        return doc;
+    }
+
+    /**
+     * Creates an xml document from source filename
+     *
+     * @param filename source filename
+     * @return xml document
+     */
+    public static Document createFromFilename(String filename) {
+        Document doc;
+        InputStream inputStream = null;
+        try {
+            inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(filename);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(inputStream);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create xml document from filename", e);
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                // Empty
+            }
+        }
+        return doc;
+    }
+}
