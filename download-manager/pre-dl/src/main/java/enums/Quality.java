2c25a08965187f74ac5f560314ab150c61fdeae2
Roi <roichenz7@gmail.com>
2014-04-21 15:06:15 +0300

    q

--- download-manager/pre-dl/src/main/java/enums/Quality.java
+++ download-manager/pre-dl/src/main/java/enums/Quality.java
@@ -1,7 +1,19 @@
 package enums;
 
 public enum Quality {
-    HD_720p,
-    HD_1080p,
-    SD
+
+    HD_720p("720p"),
+    HD_1080p("1080p"),
+    SD("");
+
+    private String name;
+
+    private Quality(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
