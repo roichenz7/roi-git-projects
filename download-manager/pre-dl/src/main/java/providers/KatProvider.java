541367945d448683c508b40b26febf55d3f2e898
Roi <roichenz7@gmail.com>
2014-04-21 15:31:27 +0300

    k

--- download-manager/pre-dl/src/main/java/providers/KatProvider.java
+++ download-manager/pre-dl/src/main/java/providers/KatProvider.java
@@ -23,11 +23,16 @@ public class KatProvider implements IProvider {
 
     @Override
     public List<ResultData> search(String tvShowName, int season, int episode, Quality quality) {
-        final String query = String.format("%s S%02dE%02d %s", tvShowName, season, episode, quality);
+        final String query = String.format("%s S%02dE%02d %s", tvShowName, season, episode, quality)
+                .replaceAll(" ", "%20");
 
         IHttpResponse response;
         try {
             response = new HttpRequestBuilder(HttpMethod.GET, getBaseUrl() + "/usearch/" + query)
+                    .withHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0")
+                    .withAccept("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+                    .withHeader("Accept-Language", "en-US,en;q=0.5")
+                    .withHeader("Accept-Encoding", "gzip, deflate")
                     .execute();
         } catch (Exception e) {
             throw new SearchException(query, e);
