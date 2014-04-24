d4d7f13dcd99c4eb4184db13d8c0584bafd6f291
Roi <roichenz7@gmail.com>
2014-04-24 23:37:57 +0300

    getStatus

--- my-episodes-api/src/test/java/MyEpisodesTest.java
+++ my-episodes-api/src/test/java/MyEpisodesTest.java
@@ -1,12 +1,21 @@
+import data.TvShowData;
 import org.junit.Test;
 import service.MyEpisodesService;
 import service.MyEpisodesServiceImpl;
+
+import java.util.Collection;
 
 public class MyEpisodesTest {
 
     @Test
     public void testLogin() {
         MyEpisodesService myEpisodesService = new MyEpisodesServiceImpl("roichenz7", "***");
+    }
+
+    @Test
+    public void testGetStatus() {
+        MyEpisodesService myEpisodesService = new MyEpisodesServiceImpl("roichenz7", "***");
+        Collection<TvShowData> status = myEpisodesService.getStatus();
     }
 
     @Test
