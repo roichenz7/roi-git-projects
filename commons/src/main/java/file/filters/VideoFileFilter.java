659ef704d2ee1fe9035ebb86067b23adc8c9a87e
Roi <roichenz7@gmail.com>
2014-04-25 15:45:11 +0300

    filter

--- commons/src/main/java/file/filters/VideoFileFilter.java
+++ commons/src/main/java/file/filters/VideoFileFilter.java
@@ -7,25 +7,26 @@ public class VideoFileFilter implements FilenameFilter {
 
     @Override
     public boolean accept(File f, String name) {
-        if (name.endsWith(".mkv"))
-            return true;
-
-        if (name.endsWith(".srt"))
-            return true;
-
-        if (name.endsWith(".mp4"))
-            return true;
-
-        if (name.endsWith(".avi"))
-            return true;
-
-        if (name.endsWith(".mpg"))
-            return true;
-
-        if (name.endsWith(".mpeg"))
-            return true;
-
-        return false;
+        return isVideoFile(name) || isSubtitleFile(name);
     }
 
+    /**
+     * @param filename filename
+     * @return true if filename describes a video file
+     */
+    public static boolean isVideoFile(String filename) {
+        return filename.endsWith(".mkv") ||
+                filename.endsWith(".mp4") ||
+                filename.endsWith(".avi") ||
+                filename.endsWith(".mpg") ||
+                filename.endsWith(".mpeg");
+    }
+
+    /**
+     * @param filename filename
+     * @return true if filename describes a subtitle file
+     */
+    public static boolean isSubtitleFile(String filename) {
+        return filename.endsWith(".srt");
+    }
 }
