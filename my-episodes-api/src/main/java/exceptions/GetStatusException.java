afc02b5427f375d0162e0ec394c42a2a554a5b91
Roi <roichenz7@gmail.com>
2014-04-25 13:23:18 +0300

    exc

--- my-episodes-api/src/main/java/exceptions/GetStatusException.java
+++ my-episodes-api/src/main/java/exceptions/GetStatusException.java
@@ -2,11 +2,11 @@ package exceptions;
 
 public class GetStatusException extends RuntimeException {
 
-    public GetStatusException() {
-        super("Failed to get status");
-    }
-
     public GetStatusException(Throwable cause) {
         super("Failed to get status", cause);
+    }
+
+    public GetStatusException(String message) {
+        super("Failed to get status. " + message);
     }
 }
