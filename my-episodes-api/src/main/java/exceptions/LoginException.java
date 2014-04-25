8c6553fc84204327f44218d2568d3d8e85593ea2
Roi <roichenz7@gmail.com>
2014-04-25 15:32:49 +0300

    login

--- my-episodes-api/src/main/java/exceptions/LoginException.java
+++ my-episodes-api/src/main/java/exceptions/LoginException.java
@@ -3,10 +3,10 @@ package exceptions;
 public class LoginException extends RuntimeException {
 
     public LoginException(String username, Throwable cause) {
-        super(String.format("Login of user: %s failed", username), cause);
+        super(String.format("Login of user '%s' failed", username), cause);
     }
 
     public LoginException(String username, String message) {
-        super(String.format("Login of user: %s failed. %s", username, message));
+        super(String.format("Login of user '%s' failed. %s", username, message));
     }
 }
