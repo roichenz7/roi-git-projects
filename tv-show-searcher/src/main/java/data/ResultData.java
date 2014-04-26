7ecf9597ac58ba2a5b7ed906aa643a24c840c823
Hen <henyarmo@gmail.com>
2014-04-26 19:31:27 +0300

    initialize + comments

--- tv-show-searcher/src/main/java/data/ResultData.java
+++ tv-show-searcher/src/main/java/data/ResultData.java
@@ -10,11 +10,11 @@ public abstract class ResultData {
     protected int episode;
     protected Quality quality;
 
-    protected int seeds;
-    protected int peers;
-
     protected String origin;
     protected boolean isProper;
+
+    protected int seeds;
+    protected int peers;
 
     protected String downloadLink;
 
@@ -22,40 +22,65 @@ public abstract class ResultData {
         initialize(source);
     }
 
-    protected abstract void initialize(Element source);
-
+    /**
+     * @return tv show name
+     */
     public String getTvShowName() {
         return tvShowName;
     }
 
+    /**
+     * @return season
+     */
     public int getSeason() {
         return season;
     }
 
+    /**
+     * @return episode
+     */
     public int getEpisode() {
         return episode;
     }
 
+    /**
+     * @return quality
+     */
     public Quality getQuality() {
         return quality;
     }
 
-    public int getSeeds() {
-        return seeds;
-    }
-
-    public int getPeers() {
-        return peers;
-    }
-
+    /**
+     * @return origin
+     */
     public String getOrigin() {
         return origin;
     }
 
+    /**
+     * @return true if this result is marked as 'proper', false otherwise
+     */
     public boolean isProper() {
         return isProper;
     }
 
+    /**
+     * @return number of seeds
+     */
+    public int getSeeds() {
+        return seeds;
+    }
+
+    /**
+     * @return number of peers
+     */
+    public int getPeers() {
+        return peers;
+    }
+
+    /**
+     * @return download link
+     */
     public String getDownloadLink() {
         return downloadLink;
     }
@@ -64,4 +89,26 @@ public abstract class ResultData {
     public String toString() {
         return String.format("%s.S%02dE%02d.%s", tvShowName, season, episode, quality);
     }
+
+    /**
+     * Initializes this result from show data
+     *
+     * @param showData show data
+     */
+    protected void initialize(ShowData showData) {
+        tvShowName = showData.getTitle();
+        season = showData.getSeasonNumber();
+        episode = showData.getEpisodeNumber();
+
+        quality = Quality.fromString(showData.getQuality());
+        origin = showData.getOrigin();
+        isProper = showData.isProper();
+    }
+
+    /**
+     * Initializes this result from source element
+     *
+     * @param source source element
+     */
+    protected abstract void initialize(Element source);
 }
