3fbf2133ea091ef6e33b9387add170e420f508b3
Roi <roichenz7@gmail.com>
2014-04-27 22:08:10 +0300

    run

--- download-manager/run/run_pre-dl.bat
+++ download-manager/run/run_pre-dl.bat
@@ -1,2 +1,2 @@
-java -cp download-manager.jar DownloadManager --pre-dl config/pre-dl-config.xml config/tv-shows.xml user password
+java -cp download-manager.jar DownloadManager --pre-dl config/pre-dl-config.xml user password
 pause
