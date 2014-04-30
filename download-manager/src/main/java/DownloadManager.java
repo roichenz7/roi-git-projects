65ada141121a885b0203542be646032e9d4dbd31
Roi <roichenz7@gmail.com>
2014-04-30 20:28:22 +0300

    mark-acquired | mark-watched

--- download-manager/src/main/java/DownloadManager.java
+++ download-manager/src/main/java/DownloadManager.java
@@ -9,7 +9,7 @@ public class DownloadManager {
             System.out.println("download-manager: incorrect amount of arguments");
             System.out.println("download-manager: usage option: --post-dl <config>");
             System.out.println("download-manager: usage option: --post-dl <config> <tv-shows-config> <username> <password>");
-            System.out.println("download-manager: usage option: --pre-dl <config> <username> <password>");
+            System.out.println("download-manager: usage option: --pre-dl [mark-acquired | mark-watched] <config> <username> <password>");
             return;
         }
 
@@ -32,14 +32,23 @@ public class DownloadManager {
                 if (args.length == 2)
                     return new PostDL(args[1]);
                 else if (args.length == 5)
-                    return new PostDL(args[1], args[2], args[3], args[4]);
+                    return new PostDL(args[1]).withMarkAsAcquired(args[2], args[3], args[4]);
                 else
                     throw new RuntimeException("Error");
             }
-            case "--pre-dl":
-                return new PreDL(args[1], args[2], args[3]);
-            default:
+            case "--pre-dl": {
+                switch (args[1]) {
+                    case "mark-acquired":
+                        return new PreDL(args[2], args[3], args[4]).withMarkAsAcquired();
+                    case "mark-watched":
+                        return new PreDL(args[2], args[3], args[4]).withMarkAsWatched();
+                    default:
+                        return new PreDL(args[1], args[2], args[3]);
+                }
+            }
+            default: {
                 throw new RuntimeException("download-manager: unknown flag (" + args[0] + ")");
+            }
         }
     }
 }
