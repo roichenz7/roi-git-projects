cb1285c60edf0de08a42ef70b7d1d06c6b0ffe1e
Roi <roichenz7@gmail.com>
2014-05-03 13:20:46 +0300

    OpenSubtitlesProvider

---
+++ tv-show-searcher/src/main/java/providers/srt/OpenSubtitlesProvider.java
@@ -0,0 +1,25 @@
+package providers.srt;
+
+import data.SearchQuery;
+import data.SearchResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OpenSubtitlesProvider implements ISrtProvider {
+
+    @Override
+    public String getName() {
+        return "OpenSubtitles";
+    }
+
+    @Override
+    public String getBaseUrl() {
+        return "http://www.opensubtitles.org";
+    }
+
+    @Override
+    public List<SearchResult> search(SearchQuery searchQuery) {
+        return new ArrayList<>(); // TODO
+    }
+}
