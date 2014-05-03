13a44a046f26b652bfa389d127f56693077676b9
Roi <roichenz7@gmail.com>
2014-05-03 13:21:56 +0300

    PodnapisiProvider

---
+++ tv-show-searcher/src/main/java/providers/srt/PodnapisiProvider.java
@@ -0,0 +1,25 @@
+package providers.srt;
+
+import data.SearchQuery;
+import data.SearchResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PodnapisiProvider implements ISrtProvider {
+
+    @Override
+    public String getName() {
+        return "Podnapisi";
+    }
+
+    @Override
+    public String getBaseUrl() {
+        return "http://www.podnapisi.net/";
+    }
+
+    @Override
+    public List<SearchResult> search(SearchQuery searchQuery) {
+        return new ArrayList<>(); // TODO
+    }
+}
