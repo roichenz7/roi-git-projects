428260059630546d679e782aa680766ce8020e5c
Roi <roichenz7@gmail.com>
2014-05-17 12:15:24 +0300

    get best result - compare quality

--- tv-show-searcher/src/main/java/providers/srt/ISrtProvider.java
+++ tv-show-searcher/src/main/java/providers/srt/ISrtProvider.java
@@ -1,5 +1,6 @@
 package providers.srt;
 
+import data.SearchQuery;
 import data.SearchResult;
 import exceptions.ResultNotFoundException;
 import providers.IProvider;
@@ -8,15 +9,11 @@ import java.util.List;
 
 public interface ISrtProvider extends IProvider {
 
-    /**
-     * Returns best result from given results
-     *
-     * @param results results
-     * @return best result
-     */
-    default SearchResult getBestResult(List<SearchResult> results) {
+    @Override
+    default SearchResult getBestResult(List<SearchResult> results, SearchQuery query) {
         return results.stream()
-                .filter(d -> acceptedOrigins.contains(d.getOrigin()))
+                .filter(x -> x.getQuality().equals(query.getQuality()))
+                .filter(x -> acceptedOrigins.contains(x.getOrigin()))
                 .findFirst()
                 .orElseThrow(ResultNotFoundException::new);
     }
--- tv-show-searcher/src/main/java/providers/torrent/ITorrentProvider.java
+++ tv-show-searcher/src/main/java/providers/torrent/ITorrentProvider.java
@@ -1,5 +1,6 @@
 package providers.torrent;
 
+import data.SearchQuery;
 import data.SearchResult;
 import exceptions.ResultNotFoundException;
 import providers.IProvider;
@@ -8,15 +9,11 @@ import java.util.List;
 
 public interface ITorrentProvider extends IProvider {
 
-    /**
-     * Returns best result from given results
-     *
-     * @param results results
-     * @return best result
-     */
-    default SearchResult getBestResult(List<SearchResult> results) {
+    @Override
+    default SearchResult getBestResult(List<SearchResult> results, SearchQuery query) {
         return results.stream()
-                .filter(d -> acceptedOrigins.contains(d.getOrigin()))
+                .filter(x -> x.getQuality().equals(query.getQuality()))
+                .filter(x -> acceptedOrigins.contains(x.getOrigin()))
                 .sorted((l, r) -> r.getSeeds() - l.getSeeds())
                 .findFirst()
                 .orElseThrow(ResultNotFoundException::new);
