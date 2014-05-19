312d77645d81c12399d5b966f61d0808ae9cd982
Roi <roichenz7@gmail.com>
2014-05-19 18:57:47 +0300

    getBestResult - using matches

--- tv-show-searcher/src/main/java/providers/srt/SrtProvider.java
+++ tv-show-searcher/src/main/java/providers/srt/SrtProvider.java
@@ -12,7 +12,7 @@ public interface SrtProvider extends Provider {
     @Override
     default SearchResult getBestResult(List<SearchResult> results, SearchQuery query) {
         return results.stream()
-                .filter(x -> x.getQuality().equals(query.getQuality()))
+                .filter(x -> x.matches(query))
                 .filter(x -> acceptedOrigins.contains(x.getOrigin()))
                 .findFirst()
                 .orElseThrow(ResultNotFoundException::new);
--- tv-show-searcher/src/main/java/providers/torrent/TorrentProvider.java
+++ tv-show-searcher/src/main/java/providers/torrent/TorrentProvider.java
@@ -12,7 +12,7 @@ public interface TorrentProvider extends Provider {
     @Override
     default SearchResult getBestResult(List<SearchResult> results, SearchQuery query) {
         return results.stream()
-                .filter(x -> x.getQuality().equals(query.getQuality()))
+                .filter(x -> x.matches(query))
                 .filter(x -> acceptedOrigins.contains(x.getOrigin()))
                 .sorted((l, r) -> r.getSeeds() - l.getSeeds())
                 .findFirst()
