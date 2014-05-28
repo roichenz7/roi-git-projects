8d1fbc3e23697dbfcefeb10354d82b66000c1c27
Roi <roichenz7@gmail.com>
2014-05-28 19:06:10 +0300

    fixed phd

--- tv-show-searcher/src/main/java/providers/torrent/results/PhdSearchResult.java
+++ tv-show-searcher/src/main/java/providers/torrent/results/PhdSearchResult.java
@@ -16,8 +16,8 @@ public class PhdSearchResult extends SearchResult {
         Elements elements = source.getElementsByTag("td");
 
         String temp = elements.get(2).getElementsByAttribute("href").get(2).attr("href");
-        String[] array = temp.replaceAll("http://", "").split("/");
-        String filename = array[3].replaceAll("\\+%5BPublicHD%5D\\.torrent", "");
+        String[] array = temp.replaceAll("http://", "").split("=");
+        String filename = array[2].replaceAll("\\+%5BPublicHD%5D\\.torrent", "");
 
         ShowData showData = ShowData.fromFilename(filename);
         initialize(showData);
@@ -25,9 +25,7 @@ public class PhdSearchResult extends SearchResult {
         seeds = Integer.parseInt(elements.get(4).text());
         peers = Integer.parseInt(elements.get(5).text());
 
-        if (temp.contains("istoretor")) {
-            String hash = array[2].toUpperCase();
-            downloadLink = "http://istoretor.com/fdown.php?hash=" + hash;
-        }
+        String hash = array[1].replaceAll("&f", "").toUpperCase();
+        downloadLink = "http://istoretor.com/fdown.php?hash=" + hash;
     }
 }
