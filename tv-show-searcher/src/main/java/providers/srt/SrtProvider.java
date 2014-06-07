9fbe0a4b73caf53b1b2bd683a4f451e881add685
Roi <roichenz7@gmail.com>
2014-06-07 21:10:13 +0300

    default impl for 'download' method

--- tv-show-searcher/src/main/java/providers/srt/SrtProvider.java
+++ tv-show-searcher/src/main/java/providers/srt/SrtProvider.java
@@ -3,6 +3,8 @@ package providers.srt;
 import data.SearchQuery;
 import data.SearchResult;
 import exceptions.ResultNotFoundException;
+import file.FileDownloader;
+import file.FileType;
 import providers.Provider;
 
 import java.util.List;
@@ -16,5 +18,10 @@ public interface SrtProvider extends Provider {
                 .filter(x -> acceptedOrigins.contains(x.getOrigin()))
                 .findFirst()
                 .orElseThrow(ResultNotFoundException::new);
+    }
+
+    @Override
+    default void download(SearchResult result) {
+        FileDownloader.downloadFile(result.getDownloadLink(), result.toString(), FileType.SRT);
     }
 }
--- tv-show-searcher/src/main/java/providers/torrent/TorrentProvider.java
+++ tv-show-searcher/src/main/java/providers/torrent/TorrentProvider.java
@@ -3,6 +3,8 @@ package providers.torrent;
 import data.SearchQuery;
 import data.SearchResult;
 import exceptions.ResultNotFoundException;
+import file.FileDownloader;
+import file.FileType;
 import providers.Provider;
 
 import java.util.List;
@@ -17,5 +19,10 @@ public interface TorrentProvider extends Provider {
                 .sorted((l, r) -> r.getSeeds() - l.getSeeds())
                 .findFirst()
                 .orElseThrow(ResultNotFoundException::new);
+    }
+
+    @Override
+    default void download(SearchResult result) {
+        FileDownloader.downloadFile(result.getDownloadLink(), result.toString(), FileType.TORRENT);
     }
 }
