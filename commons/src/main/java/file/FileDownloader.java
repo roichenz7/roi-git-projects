8edd9c235b392b44697602b27b09e40260ede1a4
Roi <roichenz7@gmail.com>
2014-06-07 22:06:12 +0300

    added 'path' param

--- commons/src/main/java/file/FileDownloader.java
+++ commons/src/main/java/file/FileDownloader.java
@@ -17,34 +17,51 @@ public final class FileDownloader {
      * Downloads file according to given download link
      *
      * @param downloadLink download link
+     * @param targetPath target path
      * @param targetFilename target file name
      * @param fileType file type
      */
-    public static void downloadFile(String downloadLink, String targetFilename, FileType fileType) {
-        downloadFile(downloadLink, targetFilename, fileType, Function.<InputStream>identity());
+    public static void downloadFile(String downloadLink,
+                                    String targetPath,
+                                    String targetFilename,
+                                    FileType fileType) {
+        downloadFile(downloadLink,
+                targetPath,
+                targetFilename,
+                fileType,
+                Function.<InputStream>identity());
     }
 
     /**
      * Downloads file according to given download link
      *
      * @param downloadLink download link
+     * @param targetPath target path
      * @param targetFilename target file name
      * @param fileType file type
      * @param inputStreamFunction input stream mapping function
      */
     public static void downloadFile(String downloadLink,
+                                    String targetPath,
                                     String targetFilename,
                                     FileType fileType,
                                     Function<InputStream, InputStream> inputStreamFunction) {
         try {
             URL url = new URL(downloadLink);
             ReadableByteChannel rbc = Channels.newChannel(inputStreamFunction.apply(url.openStream()));
-            File file = new File(targetFilename + "." + fileType);
-            if (file.exists())
-                throw new IOException("file already exists: " + file.getName());
 
-            FileOutputStream fos = new FileOutputStream(file);
+            File targetFile = new File(targetPath + "/" + targetFilename + "." + fileType);
+            if (targetFile.exists())
+                throw new IOException("file already exists: " + targetFile.getName());
+
+            File tempFile = new File(targetFilename + "." + fileType);
+            FileOutputStream fos = new FileOutputStream(tempFile);
+
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            fos.flush();
+            fos.close();
+
+            tempFile.renameTo(targetFile);
         } catch (Exception e) {
             throw new RuntimeException("Failed to download file from link: " + downloadLink, e);
         }
