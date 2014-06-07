4f39a6e6e29cabc76f32c5941d0a1bdc3224fc12
Roi <roichenz7@gmail.com>
2014-06-07 22:06:23 +0300

    adapted tests

--- commons/src/test/java/FileDownloaderTest.java
+++ commons/src/test/java/FileDownloaderTest.java
@@ -9,7 +9,7 @@ public class FileDownloaderTest {
 
     @Test
     public void test1() { // KAT link
-        FileDownloader.downloadFile("http://torcache.net/torrent/1FC4946F9C9EC781A4FC917E0528918788ECC454.torrent", "test1", FileType.TORRENT,
+        FileDownloader.downloadFile("http://torcache.net/torrent/1FC4946F9C9EC781A4FC917E0528918788ECC454.torrent", "", "test1", FileType.TORRENT,
                 x -> {
                     try {
                         return new GZIPInputStream(x);
@@ -21,6 +21,6 @@ public class FileDownloaderTest {
 
     @Test
     public void test2() { // PirateBay link
-        FileDownloader.downloadFile("http://piratebaytorrents.info/9908788/Game.Of.Thrones.S04E01.1080p.HDTV.DD5.1.x264-PublicHD.9908788.TPB.torrent", "test2", FileType.TORRENT);
+        FileDownloader.downloadFile("http://piratebaytorrents.info/9908788/Game.Of.Thrones.S04E01.1080p.HDTV.DD5.1.x264-PublicHD.9908788.TPB.torrent", ".", "test2", FileType.TORRENT);
     }
 }
