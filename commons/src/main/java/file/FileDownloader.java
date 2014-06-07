a80b7a32d24e05d1e0bdeb9c46e12d35f2715196
Roi <roichenz7@gmail.com>
2014-06-07 21:01:49 +0300

    added overloading for input stream mapping

--- commons/src/main/java/file/FileDownloader.java
+++ commons/src/main/java/file/FileDownloader.java
@@ -3,9 +3,11 @@ package file;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.function.Function;
 
 public final class FileDownloader {
 
@@ -19,9 +21,24 @@ public final class FileDownloader {
      * @param fileType file type
      */
     public static void downloadFile(String downloadLink, String targetFilename, FileType fileType) {
+        downloadFile(downloadLink, targetFilename, fileType, Function.<InputStream>identity());
+    }
+
+    /**
+     * Downloads file according to given download link
+     *
+     * @param downloadLink download link
+     * @param targetFilename target file name
+     * @param fileType file type
+     * @param inputStreamFunction input stream mapping function
+     */
+    public static void downloadFile(String downloadLink,
+                                    String targetFilename,
+                                    FileType fileType,
+                                    Function<InputStream, InputStream> inputStreamFunction) {
         try {
-            URL website = new URL(downloadLink);
-            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            URL url = new URL(downloadLink);
+            ReadableByteChannel rbc = Channels.newChannel(inputStreamFunction.apply(url.openStream()));
             File file = new File(targetFilename + "." + fileType);
             if (file.exists())
                 throw new IOException("file already exists: " + file.getName());
