947c50faac04c9ec38cdfcedd0b22134e7165bc2
Roi <roichenz7@gmail.com>
2015-02-10 21:32:46 +0200

    #EPPES-20|fixed kat url

--- tv-show-searcher/src/main/java/providers/torrent/KatProvider.java
+++ tv-show-searcher/src/main/java/providers/torrent/KatProvider.java
@@ -9,6 +9,6 @@ public class KatProvider extends KatProviderBase {
 
     @Override
     public String getBaseUrl() {
-        return "http://kickass.so";
+        return "http://kickass.to";
     }
 }
