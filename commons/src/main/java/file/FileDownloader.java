04e4b1ecbe79c78eff12a6a355f7b4485d3308d8
Roi <roichenz7@gmail.com>
2015-09-13 12:59:47 +0300

    #EPPES-43|resolved|added missing header

--- commons/src/main/java/file/FileDownloader.java
+++ commons/src/main/java/file/FileDownloader.java
@@ -72,6 +72,7 @@ public final class FileDownloader {
                     .withAccept("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
                     .withHeader("Accept-Language", "en-US,en;q=0.5")
                     .withHeader("Accept-Encoding", "gzip, deflate")
+                    .withHeader("Referer", downloadLink)
                     .execute();
             return FileUtils.gzipIfNeeded(response.getBodyAsStream());
         };
